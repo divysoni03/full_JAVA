5fc54ca8e17ab81687f8f725998cfd488d525b38
divy soni <chetanshudha267@gmail.com>
2024-08-06 23:23:06 +0530

    some aggregation task

---
+++ OPPs/constructer_task/Task17.java
@@ -0,0 +1,14 @@
+// 17. Parameterized Constructor with Default Values:
+//    Design a 'Laptop' class with fields 'brand', 'model', and 'price'. Implement a constructor that takes 'brand' and 'model' as parameters and sets a default price. Also, implement another constructor that initializes all three fields.
+
+class Laptop {
+    private String brand;
+    private String model;
+    private double price;
+}
+
+public class Task17 {
+    public static void main(String[] args) {
+        
+    }
+}

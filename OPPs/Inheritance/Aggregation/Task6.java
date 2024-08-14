a5cf2aec97637bca9be23e24d3a16dda4a8dde8a
divy soni <chetanshudha267@gmail.com>
2024-08-14 11:22:59 +0530

    some new Tasks

---
+++ OPPs/Inheritance/Aggregation/Task6.java
@@ -0,0 +1,21 @@
+// 6. Multilevel Inheritance:
+//    Develop a Java application to represent employees in an organization. Create a base class "Employee," a derived class "Manager," and another class "Director" derived from "Manager."
+
+class Employees{
+    private String employeeName;
+    private int employeeId;
+    private double salary;
+
+    public Employees(String employeeName, int employeeId, double salary) {
+        this.employeeName = employeeName;
+        this.employeeId = employeeId
+    }
+}
+class Manager {}
+class Director{}
+
+public class Task6 {
+    public static void main(String [] args) {
+
+    }
+}

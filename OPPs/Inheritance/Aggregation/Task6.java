1f50e2dae7e8d104dc7e63b7ca684f0ea310b4a9
divy soni <chetanshudha267@gmail.com>
2024-08-15 23:58:46 +0530

    some new Tasks

--- OPPs/Inheritance/Aggregation/Task6.java
+++ OPPs/Inheritance/Aggregation/Task6.java
@@ -8,10 +8,38 @@ class Employees{
 
     public Employees(String employeeName, int employeeId, double salary) {
         this.employeeName = employeeName;
-        this.employeeId = employeeId
+        this.employeeId = employeeId;
+        this.salary = salary;
+    }
+
+    public void showInfo() {
+        System.out.println("-- About Employee --");
+        System.out.println("Employee Id :" + this.employeeId);
+        System.out.println("Employee Name :" + this.employeeName);
+        System.out.println("Employee Salary :" + this.salary);
     }
 }
-class Manager {}
+class Manager extends Employees{
+    private int managerId;
+    private String managerName;
+    private double salary;
+
+    public Manager(int managerId, String managerName, double managerSalary, String employeeName, int employeeId, double salary) {
+        super(employeeName, employeeId, salary);
+        this.managerId = managerId;
+        this.managerName = managerName;
+        this.salary = managerSalary;
+    }
+
+    public void showInfo() {
+        System.out.println("-- About Manager --");
+        System.out.println("Manager Id :" + this.managerId);
+        System.out.println("Manager Name :"  + this.managerName);
+        System.out.println("Manager Salary :" + this.salary);
+        System.out.println("-- Employee Under This Manager --");
+        super.showInfo();
+    }
+}
 class Director{}
 
 public class Task6 {

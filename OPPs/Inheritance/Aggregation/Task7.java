5c4e17e93d1f062b3871e292041c354c2d70cfd0
divy soni <chetanshudha267@gmail.com>
2024-08-16 20:01:40 +0530

    Task 7

---
+++ OPPs/Inheritance/Aggregation/Task7.java
@@ -0,0 +1,11 @@
+// 7. Hierarchical Inheritance:
+//    Design a Java system for modeling different types of animals with a base class "Animal" and two derived classes "Mammal" and "Bird" to showcase hierarchical inheritance.
+class Animal{}
+class Mammal extends Animal{}
+class Bird extends Animal{}
+
+public class Task7 {
+    public static void main(String []args) {
+
+    }
+}

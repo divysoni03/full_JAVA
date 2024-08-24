d6a92518e0653ecbf4319fa13664c6354ce14ee9
divy soni <chetanshudha267@gmail.com>
2024-08-24 11:07:15 +0530

    task 9, 10

---
+++ OPPs/Inheritance/Aggregation/Task10.java
@@ -0,0 +1,33 @@
+// 10. Multilevel Inheritance:
+//     Develop a Java application for a vehicle rental system, with a base class "Vehicle," a derived class "Car," and another class "LuxuryCar" derived from "Car" to demonstrate multilevel inheritance.
+
+import javax.swing.tree.VariableHeightLayoutCache;
+
+class Vehicle {
+    private String name;
+    private double price;
+    private boolean isAvailable;
+    public Vehicle(String name, double price) {
+        this.name = name;
+        this.price = price;
+        this.isAvailable = false;
+    }
+
+    public void showInfo() {
+        System.out.println("-- About Vehicle --");
+        System.out.println("Vehicle Name : " + this.name);
+        System.out.println("Vehicle Price : " + this.price);
+        System.out.println("Vehicle availability : " + (this.isAvailable? "Yes":"No"));
+    }
+}
+class Car extends Vehicle{ 
+    private String model;
+        
+}
+class LuxuryCar{}
+
+public class Task10 {
+    public static void main(String []args) {
+
+    }    
+}

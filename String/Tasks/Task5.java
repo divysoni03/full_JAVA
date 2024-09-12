cdaa14b867b40c4ff670f45ef03bf9e9a5c73433
divy soni <chetanshudha267@gmail.com>
2024-09-12 22:46:27 +0530

    changes

--- String/Tasks/Task5.java
+++ String/Tasks/Task5.java
@@ -2,5 +2,7 @@
 //    Build a secure authentication system using hashed passwords in Java, where users' passwords are stored as hashes and compared during login verification.
 
 public class Task5 {
-    
+    public static void main(String []args) {
+        
+    }
 }
--- String/Tasks/Task6.java
+++ String/Tasks/Task6.java
@@ -1,6 +1,23 @@
 // 6. String Manipulation:
 //    Create a Java program that takes a user's input and formats it into a readable sentence, ensuring proper capitalization and removing leading/trailing spaces.
 
+import java.util.Scanner;
+
 public class Task6 {
-    public static void main(String []args) {}
+    public static void main(String []args) {
+        Scanner sc = new Scanner(System.in);
+        String str;
+        System.out.println("Enter A String :");
+        str = sc.nextLine();
+        String word = "";
+        for(char ch: str.toCharArray()) {
+            if(ch == ' ') {
+                if(!word.isEmpty()) {
+                    if(word.charAt(0) >= 'a' && word.charAt(0) <= 'z') {
+                        word.charAt(0) = (word.charAt(0) - 'a') + 'A';
+                    }
+                }
+            }
+        }
+    }
 }
